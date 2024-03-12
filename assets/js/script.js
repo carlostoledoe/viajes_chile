04547f9c03a39661886de18e10030baa48503d3d
Carlos Toledo <carlombo@hotmail.com>
2024-03-11 22:44:36 -0300

    Se realizan actualizaciones de estilo y se configura navbar para cambiar de color al hacer scrolling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,28 @@
 // Tootips
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+//Scroll Reveal
+window.sr = ScrollReveal();
+sr.reveal('#quienes-somos', {
+    duration: 2000,
+    delay: 200
+});
+
+// Cambio de color al navbar:
+
+// Añade un evento de escucha para el evento de scroll
+window.addEventListener('scroll', function() {
+  // Obtiene la altura del scroll actual
+  const scrollPosition = window.scrollY;
+
+  // Obtiene el navbar por su ID
+  const navbar = document.getElementById('naavbarr');
+
+  // Si la posición del scroll es mayor a 50px, cambia el color
+  if (scrollPosition > 700) {
+    navbar.classList.add('navbar-color-change');
+  } else {
+    navbar.classList.remove('navbar-color-change');
+  }
+});
